d072eed4fdb9e2be10ddcdd7708a8535623ba74e
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-09-21 23:58:06 +0500

    updated

--- Python For Data Science/1_intro_to_python.py
+++ Python For Data Science/1_intro_to_python.py
@@ -1,1 +1,18 @@
 print("Welome to the complete data science course, organized by Professor Raimal Raja")
+
+'''
+
+1 - Open source general purpose programming language,
+2 - Object Oriented
+3 - Programming as well as scripting langauge
+
+Python is a general-purpose programming language that is often applied in scripting roles
+
+Features
+1 - Easy to learn & use
+2 - Interpreted langauage
+3 - Large standard Library
+4 - Large Community support
+5 - Extensible
+6 - Cross-platform langauge
+'''
