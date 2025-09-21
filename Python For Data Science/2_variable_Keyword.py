e5fa6ca5e460a9ae5f1375cfe87dff821cca4dcc
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-09-22 00:08:39 +0500

    updated

---
+++ Python For Data Science/2_variable_Keyword.py
@@ -0,0 +1,26 @@
+'''
+
+Variables
+A temporary storage space where you can keep changing values.
+assigning values to a variable:
+1 - To  assign values to a vaiable in python, we will use the assignment(=) operator.
+
+a = 10, a = 'Welcome to the class'
+
+2 - No need to declare the datatype of the variables like other programming languages
+
+Keywords
+special reserved words which convey a special meaning to interpeter or compiler.
+1 - it can't be used as a variable.
+2 - few of the example keywords are fellows:
+1 - def, else, if, class, continue, break, finall, from, lambda, except, import, none
+
+
+
+'''
+
+a = 'Professor'
+print(a)
+
+type(a)
+print(type(a))
