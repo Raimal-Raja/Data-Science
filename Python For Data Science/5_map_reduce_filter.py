13686d46e2b952c492ee9ed9b4284e949ee95e83
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-09-22 02:31:55 +0500

    Update 5_map_reduce_filter.py

--- Python For Data Science/5_map_reduce_filter.py
+++ Python For Data Science/5_map_reduce_filter.py
@@ -15,5 +15,12 @@ print(areas)
 
 # map function
 
+
 a = list(map(area,radi))
 print(a)
+
+city = [("badin", 23), ("khoski", 32)]
+
+cel_to_f = lambda data: (data[0], (9/5)* data[1]+32)
+f = list(map(cel_to_f, city))
+print(f)
