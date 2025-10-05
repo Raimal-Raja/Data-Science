c1161b74db67654c7ac84783dea90ef14575808d
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-10-05 08:37:35 +0500

    updated

--- Python For Data Science/6_fileHandling.py
+++ Python For Data Science/6_fileHandling.py
@@ -1,4 +1,16 @@
 file = open(r'D:\GitHub\Data-Science\Python For Data Science\file.txt', 'r')
 
-for  a in file:
-    print(a)
+# for  a in file:
+#     print(a)
+
+print(file.read())
+
+
+## Write a file
+
+file2 =  open('file2.txt','w')
+file2.write('I love you my kavita')
+
+file2 = open('file2.txt','r')
+
+print(file2.read())
