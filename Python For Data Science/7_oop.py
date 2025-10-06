db91750d35e1c3ff62974f25523acbfad8d96c2b
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-10-07 01:07:24 +0500

    initial commit

---
+++ Python For Data Science/7_oop.py
@@ -0,0 +1,18 @@
+# class Rectange:
+#     def __init__(self):
+#         self.length = 10
+#         self.breadth = 5
+
+
+
+
+class Rectange:
+    def __init__(self, length, breadth):
+        self.length = length
+        self.breadth = breadth
+        
+# instantiating a class
+rect = Rectange(5,6)
+
+# calling the instance variable
+rect.breadth
