c692f366d1107f7a45f351bba5aa84d53b85088e
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-10-18 21:06:26 +0500

    updated

--- Python For Data Science/11_matplotlib.py
+++ Python For Data Science/11_matplotlib.py
@@ -1,3 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+
+data =  {
+    'Year':[1920,1930,1940,1950,1970,1980,1990,2000,2010,2020],
+    'Exchange Rate': [65,69,71,64,62,59,72,71,75,78]
+}
+
+
+# print(type(data))
+df = pd.DataFrame(data)
+
+
+#explicit mention  column 
+df  = pd.DataFrame(data, columns=['Year', 'Exchange Rate'])
+# print(df)
+
+# print(type(df))
+
+# new_df = pd.read_clipboard()
+# print(new_df)
+
+new_df = pd.read_csv('D:\GitHub\Data-Science\Python For Data Science\company_dataset.csv')
+print(new_df)
